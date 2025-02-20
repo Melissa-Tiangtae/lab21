eac3d4b828ab171d0021db04e6b956fb7cadc770
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2025-02-21 01:33:47 +0700

    average_completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc, char *argv[]){
+    
+    double sum = 0.0;
+    double ans;
+   
+
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }else{
+
+      for(int i =1; i<argc; i++){
+        sum += atof(argv[i]);
+      }
+
+      ans = (sum/(argc-1.0));
+
+    cout << "---------------------------------" << "\n";
+    cout << "Average of "<< argc-1 <<" numbers = " << ans << "\n";
+    cout << "---------------------------------";
+    
+    }
+
+
+    return 0;
+}
